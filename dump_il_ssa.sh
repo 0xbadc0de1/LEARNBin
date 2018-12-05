c82561c7b1b48cf6bd6759e1b00c29bc365a298f
0xbadc0de1 <1337@1337.com>
2018-12-05 23:05:20 +0100

    bil+ssa dumper.

---
+++ dump_il_ssa.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+bap $1 -d --ssa > dump_il_ssa.txt
--- traverse_binary.py
+++ traverse_binary.py
@@ -3,9 +3,14 @@ import bap
 
 if __name__ == "__main__":
    
-    test_binary = "/bin/ls"
-    proj = bap.run(test_binary)
+    test_binary = "/bin/true"
+    print "parsing..."
+    proj = bap.run(test_binary, ['--ssa'])
+    
+    print "run complete."
     for sub in proj.program.subs:
         print sub.name
         for p in sub.blks:
             print p
+
+
